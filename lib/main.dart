4630be0d3a0cd983eb52443e20ba9f53c7319e2d
Teniel Smith-Edwards <tsmith1227@outlook.com>
2025-04-04 12:29:46 -0400

    added API and updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+//imported needed libraries
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -30,8 +33,29 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final TextEditingController _cadCon = TextEditingController();
   final TextEditingController _usdCon = TextEditingController();
-  final double rate = 1.35;
+  double rate = 1.35; // For static exchange rate implementation - add final
+  // if using api - double rate = 1.35
 
+  // API Implementation
+  @override
+  void initState() {
+    super.initState();
+    fetchRate();
+  }
+
+  Future<void> fetchRate() async {
+    final uri = Uri.parse('https://open.er-api.com/v6/latest/USD');
+    final res = await http.get(uri);
+
+    if (res.statusCode == 200) {
+      final r = json.decode(res.body);
+      setState(() {
+        rate = r['rates']['CAD'];
+      });
+    }
+  }
+
+  // conversion methods
   void _convertToCAD(String val) {
     if (val.isNotEmpty){
       double usd = double.tryParse(val) ?? 0;
@@ -46,6 +70,7 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
+  // navigtion method
   void _navigateToSummaryPage () {
     Navigator.push(
       context, 
@@ -58,25 +83,26 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Currency Converter Page")),
+      backgroundColor: const Color.fromARGB(166, 200, 178, 239),
+      appBar: AppBar(title: Text("Currency Converter Page"), backgroundColor: const Color.fromARGB(180, 200, 178, 239)),
       body: Padding(padding: const EdgeInsets.all(16.0), 
         child: Column(
           children: [
-            Text("Welcome user to Teniel's Currency Converter App!"), 
-            TextField(
+            Text("Welcome user to Teniel's Currency Converter App!", style: TextStyle(color: Colors.deepPurple, fontSize: 25.0)), 
+            TextFormField(
               controller: _cadCon,
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               decoration: InputDecoration(label: Text('CAD: ')),
               onChanged: _convertToUSD,
             ), 
-            SizedBox(height: 16,), 
-            TextField(
+            SizedBox(height: 20), 
+            TextFormField(
               controller: _usdCon,
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               decoration: InputDecoration(label: Text('USD: ')),
               onChanged: _convertToCAD,
             ), 
-            SizedBox(height: 16,), 
+            SizedBox(height: 20), 
             ElevatedButton(onPressed: _navigateToSummaryPage, child: Text("View Summary")),
           ],
         ),
@@ -95,14 +121,21 @@ class SummaryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Conversion Summary Page")),
+      backgroundColor: const Color.fromARGB(166, 200, 178, 239),
+      appBar: AppBar(title: Text("Conversion Summary Page"), backgroundColor: const Color.fromARGB(180, 200, 178, 239)),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Text("CAD: $cad", style: TextStyle(fontSize: 20)),
-            Text("USD: $usd", style: TextStyle(fontSize: 20)),
-            SizedBox(height: 16),
+            Center(
+              child: Text("Summary", style: TextStyle(color: Colors.deepPurple, fontSize: 45.0)),
+            ),
+            Text("CAD: $cad", style: TextStyle(color: Colors.deepPurple, fontSize: 30.0)),
+            SizedBox(height: 5),
+            Text("USD: $usd", style: TextStyle(color: Colors.deepPurple, fontSize: 30.0)),
+            SizedBox(height: 5),
+            Text("Exchange Rate: 1 USD = \$$rate CAD", style: TextStyle(color: Colors.deepPurple, fontSize: 20.0)),
+            SizedBox(height: 25),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
